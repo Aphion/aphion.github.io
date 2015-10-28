647029b444628d13eacb79c61920e9407a1d5cac
Aphion <nrtf526@outlook.com>
2015-10-28 22:27:22 +0000

    added some of basic file\dir structure

--- packages.sh
+++ packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/infinitheme debs/Infinitheme
-dpkg-deb -bZgzip projects/retroarch debs/RetroArch/iOS8.4Below
+dpkg-deb -bZgzip projects/retroarch8.4Below debs/RetroArch/iOS8.4Below
+dpkg-deb -bZgzip projects/retroarch9Above debs/RetroArch/iOS9Above
 # dpkg-deb -bZgzip projects/<project name> <output folder>
