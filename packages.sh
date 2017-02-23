b0ce44ee780be397b2761561ad9d8667107c3431
Aphion <nrtf526@outlook.com>
2017-02-23 11:16:52 -0600

    Commented out .deb creation in packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dpkg-deb -bZgzip Projects/InfiniTheme Debs/InfiniTheme
-dpkg-deb -bZgzip Projects/RetroArch8Below Debs/RetroArch8Below
-dpkg-deb -bZgzip Projects/RetroArch9Above Debs/RetroArch9Above
+#dpkg-deb -bZgzip Projects/InfiniTheme Debs/InfiniTheme
+#dpkg-deb -bZgzip Projects/RetroArch8Below Debs/RetroArch8Below
+#dpkg-deb -bZgzip Projects/RetroArch9Above Debs/RetroArch9Above
 # dpkg-deb -bZgzip Projects/<project name> <output folder>
